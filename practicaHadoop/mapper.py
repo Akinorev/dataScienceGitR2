d8b41d93377badda129ea59f7e70863fb19aff4a
evergom <veronica.gomez.gomez@ericsson.com>
2019-11-26 13:55:14 +0100

    Added first version of city control

--- practicaHadoop/mapper.py
+++ practicaHadoop/mapper.py
@@ -17,21 +17,30 @@ for line in fileDict:
 #   print scores.items()
 
 
-# Read each line from STDIN
-for line in sys.stdin:
+file = sys.stdin
 
+#print [obj for obj in file if(obj['country_code'] == 'UY')]
+
+
+# Read each line from STDIN
+#for line in sys.stdin:
+for line in file:
+
+   if '"country_code":"US"' in line:
+   #Filter here, each line still consist on Json format
+      jsonLine = json.loads(line)
    # Get the words in each line
-   words = line.split()
+      words = line.split()
 
    # Generate the count for each word
-   for word in words:
-
+      for word in words:
+ 
       # Write the key-value pair to STDOUT to be processed by the reducer.
-      # The key is anything before the first tab character and the value is
-      # anything after the first tab character.
-      if word in scores:
-         print '{0}\t{1}'.format(word,scores[word])
-      else:
-         print '{0}\t{1}'.format(word,0)
+      # First we compare if the word appears on the dictionary
+      # If it appears we give it corresponding value, if not we just give zero value
+         if word in scores:
+            print '{0}\t{1}'.format(word,scores[word])
+         else:
+            print '{0}\t{1}'.format(word,0)
 
 #cat sherlock.txt | ./mapper.py | sort -t 1 | ./reducer.py
