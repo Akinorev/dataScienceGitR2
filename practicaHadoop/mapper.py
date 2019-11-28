722d1c808e5ce1f322b0294f20000c51c97f96c4
evergom <veronica.gomez.gomez@ericsson.com>
2019-11-28 14:16:45 +0100

    working on the options flags for hashtags and countries

--- practicaHadoop/mapper.py
+++ practicaHadoop/mapper.py
@@ -4,6 +4,8 @@ import sys
 import json
 import string
 import re
+from argparse import ArgumentParser
+
 
 sys.path.append('.')
 
@@ -20,28 +22,53 @@ for line in fileDict:
 file = sys.stdin
 
 #print [obj for obj in file if(obj['country_code'] == 'UY')]
+#parser = ArgumentParser()
+
+#parser.add_argument('-c','--country', type=str, help='Filter tweets by country code (example: UY')
+#parser.add_argument('-h','--hashtag', type=str, help='Filter tweets by hashtag (example: #helloTwitter)')
+
+#args = parser.parse_args()
+
+#filterCountry = ""
+#filterHashtag = ""
+
+#if (args.country != ""):
+#   filterCountry = '"country_code":"' + str(args.country) + '"'
+
+#if (args.hashtag != ""):
+#   filterHastag = str(args.hashtag)
+
+
 
 
 # Read each line from STDIN
 #for line in sys.stdin:
 for line in file:
 
+   jsonLine = ""
+
    # NEXT IF ONLY FOR EXTRA POINTS, USE OUTSIDE BETTER
-   #if '"country_code":"US"' in line:
-   #Filter here, each line still consist on Json format
-      jsonLine = json.loads(line)
+ #  if (filterCountry != ""):   
+ #     if filterCountry in line:
+ #        jsonLine = line
+ #        print jsonLine
+ #  elif (filterHastag != ""):
+ #     if filterHastag in line:
+ #        jsonLine = line
+ #  else:
+   jsonLine = line
    # Get the words in each line
-      words = line.split()
+   words = jsonLine.split()
 
    # Generate the count for each word
-      for word in words:
+   for word in words:
  
       # Write the key-value pair to STDOUT to be processed by the reducer.
       # First we compare if the word appears on the dictionary
       # If it appears we give it corresponding value, if not we just give zero value
-         if word in scores:
-            print '{0}\t{1}'.format(word,scores[word])
-         else:
-            print '{0}\t{1}'.format(word,0)
+      if word in scores:
+         print '{0}\t{1}'.format(word,scores[word])
+      else:
+         print '{0}\t{1}'.format(word,0)
 
 #cat sherlock.txt | ./mapper.py | sort -t 1 | ./reducer.py
